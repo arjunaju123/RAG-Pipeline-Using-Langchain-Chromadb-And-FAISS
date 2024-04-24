d39eff5061ca1fd86ef7f3faf4f36b486a9b29a0
arjunaju123 <54721arjun@gmail.com>
2024-04-24 17:37:29 +0530

    Building Gen AI Powered App Using Langchain And Huggingface And Mistral

--- groq/app.py
+++ groq/app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import os
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import WebBaseLoader
-from langchain.embeddings import OllamaEmbeddings
+# from langchain.embeddings import OllamaEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
@@ -22,12 +23,15 @@ if "vector" not in st.session_state:
     st.session_state.docs=st.session_state.loader.load()
 
     st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
-    st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
+    st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:2])
     st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
 
 st.title("ChatGroq Demo")
 llm=ChatGroq(groq_api_key=groq_api_key,
-             model_name="mixtral-8x7b-32768")
+             model_name="Gemma-7b-It")
+
+#Gemma-7b-It
+#mixtral-8x7b-32768
 
 prompt=ChatPromptTemplate.from_template(
 """
